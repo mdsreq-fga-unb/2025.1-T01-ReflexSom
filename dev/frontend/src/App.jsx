203f1e7cec54f727ff017662d23e93098d83471a
luluaroeira <luluaroeira@gmail.com>
2025-06-27 14:00:00 -0300

    Adicionando configuraçoes do aplicativo

--- dev/frontend/src/App.jsx
+++ dev/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import { CartProvider } from './contexts/CartContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import StaffRoute from './components/StaffRoute';
 import Login from './components/Login';
@@ -9,53 +10,72 @@ import Dashboard from './components/Dashboard';
 import Perfil from './components/Perfil';
 import Equipamentos from './components/Equipamentos';
 import CadastrarEquipamento from './components/CadastrarEquipamento';
+import DetalhesEquipamento from './components/DetalhesEquipamento';
+import Carrinho from './components/Carrinho';
+import AdminReservas from './components/AdminReservas';
 import './App.css';
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <div className="App">
-          <Routes>
-            {/* Rotas públicas */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/registro" element={<Registro />} />
-            
-            {/* Rotas protegidas */}
-            <Route path="/dashboard" element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            } />
-            
-            <Route path="/perfil" element={
-              <ProtectedRoute>
-                <Perfil />
-              </ProtectedRoute>
-            } />
-            
-            <Route path="/equipamentos" element={
-              <ProtectedRoute>
-                <Equipamentos />
-              </ProtectedRoute>
-            } />
-            
-            <Route path="/equipamentos/cadastrar" element={
-              <ProtectedRoute>
+      <CartProvider>
+        <Router>
+          <div className="App">
+            <Routes>
+              {/* Rotas públicas */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/registro" element={<Registro />} />
+              
+              {/* Rotas protegidas */}
+              <Route path="/dashboard" element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              } />
+              
+              <Route path="/perfil" element={
+                <ProtectedRoute>
+                  <Perfil />
+                </ProtectedRoute>
+              } />
+              
+              <Route path="/equipamentos" element={
+                <ProtectedRoute>
+                  <Equipamentos />
+                </ProtectedRoute>
+              } />
+              
+              <Route path="/equipamentos/:id" element={
+                <ProtectedRoute>
+                  <DetalhesEquipamento />
+                </ProtectedRoute>
+              } />
+              
+              <Route path="/carrinho" element={
+                <ProtectedRoute>
+                  <Carrinho />
+                </ProtectedRoute>
+              } />
+              
+              {/* Rotas protegidas para staff */}
+              <Route path="/equipamentos/cadastrar" element={
                 <StaffRoute>
                   <CadastrarEquipamento />
                 </StaffRoute>
-              </ProtectedRoute>
-            } />
-            
-            {/* Rota padrão - redireciona para dashboard se autenticado, senão para login */}
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            
-            {/* Rota 404 - redireciona para dashboard */}
-            <Route path="*" element={<Navigate to="/dashboard" replace />} />
-          </Routes>
-        </div>
-      </Router>
+              } />
+              
+              <Route path="/admin/reservas" element={
+                <StaffRoute>
+                  <AdminReservas />
+                </StaffRoute>
+              } />
+              
+              {/* Redirecionamento padrão */}
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            </Routes>
+          </div>
+        </Router>
+      </CartProvider>
     </AuthProvider>
   );
 }
